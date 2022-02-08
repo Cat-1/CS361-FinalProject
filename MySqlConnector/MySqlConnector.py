b80aebd3392a5f4849f24afa6a5d437ddf62ba56
Cat-1 <17724917+Cat-1@users.noreply.github.com>
2022-02-07 22:31:20 -0500

    MySQL Connector SErvice

---
+++ MySqlConnector/MySqlConnector.py
@@ -0,0 +1,51 @@
+import yaml
+import mysql.connector
+from flask import Flask, jsonify, request
+import json
+
+try:
+    from yaml import CLoader as loader, CDumper as dumper
+except ImportError:
+    from yaml import loader, dumper
+stream = open("config.yaml")
+config = yaml.load(stream, loader)
+
+conn = mysql.connector.connect(**config["MySql"])
+
+app = Flask(__name__)
+
+
+def JsonifyMySql(columns, data):
+    result = []
+    columnHeader = []
+    for i in range(len(columns)):
+        columnName = columns[i]
+        columnHeader.append(columnName)
+
+    for row in range(len(data)):
+        obj = {}
+        for column in range(len(data[row])):
+            obj[columnHeader[column]] = data[row][column]
+        result.append(obj)
+    return jsonify(result)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    body = request.get_json()
+    db = conn.cursor()
+    param = body["Parameters"]
+    if body["Parameters"]:
+        db.execute(body["Query"], param)
+    else:
+        db.execute(body["Query"])
+    rows = db.fetchall()
+    columnHeaders = db.column_names
+    return JsonifyMySql(columnHeaders, rows)
+
+try:
+    app.run()
+except:
+    print("Exception")
+finally:
+    conn.close()
